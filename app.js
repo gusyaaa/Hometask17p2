b4fc08d7ef91b558a3096e44763d70f7c496952f
gusyaaa <askerzadehuseyn2005@gmail.com>
2024-06-19 15:54:27 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+let text = "Donald Trump served as the 45th President of the United States from 2017 to 2021. Before his presidency, he was a well-known real estate developer and television personality.";
+console.log(text.length);
+console.log(text.includes("e"));
+console.log(text.startsWith("code"));
+console.log(text.endsWith("code"));
+console.log(text.replaceAll("t", "####"));
